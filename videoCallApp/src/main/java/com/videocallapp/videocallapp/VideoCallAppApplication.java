2f830fc265f35b32dc4b2478cf787a43f627d38b
Soumyakhi <soumyajitwwe@gmail.com>
2025-01-15 00:43:53 +0530

    first

---
+++ videoCallApp/src/main/java/com/videocallapp/videocallapp/VideoCallAppApplication.java
@@ -0,0 +1,13 @@
+package com.videocallapp.videocallapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCallAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoCallAppApplication.class, args);
+    }
+
+}
